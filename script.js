2963265b1d87adf28d9abf314202c81deab6b583
MisShubham <166797650+MisShubham@users.noreply.github.com>
2024-06-07 14:20:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const validCredentials = [
-    { email: 'shubham.mis@klhomeappliances.com', password: 'Shubham@123' },
-    { email: 'another.email@example.com', password: 'AnotherPassword' }
-    // Add more valid credentials as needed
+    { email: 'sachin.dsp@klhomeappliances.com', password: 'Sachin@dspKL', website: 'https://sites.google.com/klhomeappliances.com/store-in-charge?usp=sharing' },
+    { email: 'subroto@klhomeappliances.com', password: 'Subroto@MISKL', website: 'https://sites.google.com/klhomeappliances.com/subroto-sir?usp=sharing' },
+    { email: 'komal.sc@klhomeappliances.com', password: 'Komal', website: 'https://sites.google.com/klhomeappliances.com/crmdashboard?usp=sharing' },
+    { email: 'sakshisingh2707@gmail.com', password: 'Sakshi@KL', website: 'https://sites.google.com/klhomeappliances.com/somudeep-mahto?usp=sharing' },
+    { email: 'dkpal1399@gmail.com', password: 'Dkpal@KL', website: 'https://sites.google.com/klhomeappliances.com/dinesh-sir?usp=sharing' },
+    { email: 'rahul2011.kumarmehta@gmail.com', password: 'Rahul@KL', website: 'https://sites.google.com/klhomeappliances.com/rahul-sir?usp=sharing' },
+    { email: 'sonia@klhomeappliances.com', password: 'Sonia@KL', website: 'https://sites.google.com/klhomeappliances.com/sonia?usp=sharing' },
+    { email: 'sanjay.purchase@klhomeappliances.com', password: 'Sanjay@KL', website: 'https://sites.google.com/klhomeappliances.com/sanjay?usp=sharing' },
+    { email: 'deepak@klhomeappliances.com', password: 'Deepak@KL', website: 'https://sites.google.com/klhomeappliances.com/deepak?usp=sharing' },
+    { email: 'shivaniklhomeappliances.com', password: 'Shivani', website: 'https://sites.google.com/klhomeappliances.com/shivani?usp=sharing' },
+    { email: 'rajnicrm@klhomeapplinces.com', password: 'Rajni@KL', website: 'https://sites.google.com/klhomeappliances.com/rajni?usp=sharing' },
+    { email: 'md@klhomeappliances.com', password: 'Shivam@md', website: 'https://sites.google.com/klhomeappliances.com/ajay?usp=sharing' },
+    { email: 'rita@klhomeappliances.com', password: 'RitaMishra123@KL', website: 'https://sites.google.com/klhomeappliances.com/rita?usp=sharing' },
+    { email: 'kusum@klhomeappliances.com', password: 'Kusum', website: 'https://sites.google.com/klhomeappliances.com/process-cordinator?usp=sharing' }
 ];
 
 function handleLogin(event) {
@@ -11,14 +22,15 @@ function handleLogin(event) {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
     
-    const isValid = validCredentials.some(credential => 
+    const user = validCredentials.find(credential => 
         credential.email.toLowerCase() === email && credential.password === password
     );
     
-    if (isValid) {
-        window.location.href = 'https://www.google.com';
+    if (user) {
+        window.location.href = user.website;
     } else {
         errorMessage.textContent = 'Invalid email or password';
         errorMessage.style.display = 'block';
     }
 }
+
