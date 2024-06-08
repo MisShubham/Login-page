c4b65da30227dc8f47594eb02f78280fbb5c4536
MisShubham <166797650+MisShubham@users.noreply.github.com>
2024-06-08 17:29:47 +0530

    script.js

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const validCredentials = [
     { email: 'rita@klhomeappliances.com', password: 'RitaMishra123@KL', website: 'https://sites.google.com/klhomeappliances.com/rita?usp=sharing' },
     { email: 'kusum@klhomeappliances.com', password: 'Kusum', website: 'https://sites.google.com/klhomeappliances.com/process-cordinator?usp=sharing' },
     { email: 'tanuja@klhomeappliances.com', password: 'Tanuja@KL', website: 'https://sites.google.com/klhomeappliances.com/kl-home-appliances?usp=sharing' },
-    { email: 'hr@klhomeappliances.com', password: 'Saloni@12KL', website: 'https://sites.google.com/klhomeappliances.com/kl-home-appliances-saloni?usp=sharing' }
+    { email: 'hr@klhomeappliances.com', password: 'Saloni@12KL', website: 'https://sites.google.com/klhomeappliances.com/kl-home-appliances-saloni?usp=sharing' },
+    { email: 'khushikumari@klhomeappliances.com', password: 'Khushi@123KL', website: 'https://sites.google.com/klhomeappliances.com/kl-home-appliances-deo?usp=sharing' }
 ];
 
 function handleLogin(event) {
